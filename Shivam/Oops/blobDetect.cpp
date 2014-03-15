91c1eeb3404898c03aa1fb412e3a87bfc2904598
Shivam Vats <aries@aries-Inspiron-3521.(none)>
2014-03-15 09:35:08 +0530

    Deleted

--- Shivam/Oops/blobDetect.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-Mat binary(Mat img,int threshold)
-{
-	img=cv::imread(/home/aries/Desktop/image.jpg,0);
-	
-}
--- Shivam/Oops/node.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>	
-
-
-class node 
-{ public:
-    int x; 
-    int y; 
-    class node *next; 
-};
