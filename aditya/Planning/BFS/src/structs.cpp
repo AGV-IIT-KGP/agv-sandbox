6e9251596089ba87d3e63c10dd181fac75d37372
aditya <narayanaditya95@gmail.com>
2014-03-30 02:05:09 +0530

    ASTAR ON OPENCV

--- aditya/Planning/BFS/src/structs.cpp
+++ aditya/Planning/BFS/src/structs.cpp
@@ -37,50 +37,51 @@ public:
 		if(rear<lim)
 			q[rear++]=a;
 		else
-			std::cout<<"Overflow";
+	std::cout<<"Overflow";
 	}
 	point deque()
-	{
-		if(rear==0){
-			std::cout<<"Empty queue";
-		}
-		int i;
-		point first=q[0];
-		for(i=0;i<rear-1;++i)
-			q[i]=q[i+1];
-		rear--;
-		return first;
+	{	
+	if(rear==0){
+		std::cout<<"Empty queue";
+	}
+	int i;
+	point first=q[0];
+	for(i=0;i<rear-1;++i)
+		q[i]=q[i+1];
+	rear--;
+	return first;
 	}
 };
 
 class map
 {
 private:
-	int i,j,x,y;
-	int **array;
+	int i,j;
+int **array;
 public:
-	map(int n,int m){
-		x=n;
-		y=m;
-		array = new int*[n];
-		for(i=0;i<n;++i)
-    		array[i] = new int[m];
-	}
-	void clear(){
-		for(i=0;i<x;++i)
-			for(j=0;j<y;++j)
-				array[i][j]=0;
-	}
-	void set(point p,int a){
-		array[p.x][p.y]=a;
-	}
-	int look(int x,int y){
-		return array[x][y];
-	}
-	// void print(){
-	// 	for(i=0;i<size;++i){
-	// 		for(j=0;j<size;++j)
-	// 			std::cout<<array[i][j]<<"\t";
-	// 		std::cout<<std::endl<<std::endl;
-	// 	}}
+int size;
+map(int n){
+array = new int*[n];
+for(i=0;i<n;++i)
+     array[i] = new int[n];
+size=n;
+}
+void clear(){
+for(i=0;i<size;++i)
+for(j=0;j<size;++j)
+array[i][j]=0;
+}
+void set(point p,int a){
+array[p.x][p.y]=a;
+}
+int look(int x,int y){
+return array[x][y];
+}
+void print(){
+for(i=0;i<size;++i){
+for(j=0;j<size;++j)
+std::cout<<array[i][j]<<"\t";
+std::cout<<std::endl<<std::endl;
+}
+}
 };
